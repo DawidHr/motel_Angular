28ba567e00a89adae332573d621700c0ee6aed6a
unknown <dawiid21@gmail.com>
2019-07-02 22:38:12 +0200

    Modified reservation filter

--- motel/src/app/reservation/reservation.component.ts
+++ motel/src/app/reservation/reservation.component.ts
@@ -12,11 +12,20 @@ import { CustomerService } from '../object/customer.service';
 export class ReservationComponent implements OnInit {
 
   reservationList: Reservation[] = [];
+  data_start : String = "";
+  data_end: String = "";
 
   constructor(private reservationService: ReservationService, private customerService: CustomerService) { }
 
   ngOnInit() {
-    this.reservationList = this.reservationService.reservationList;
+   // this.reservationList = this.reservationService.reservationList;
+    var start_date = new Date();
+    for(var i=0;i<this.reservationService.reservationList.length;i++) {
+      if(this.reservationService.reservationList[i].start_data >= start_date) {
+        this.reservationList.push(this.reservationService.reservationList[i]);
+      }
+    }
+
   }
 
   getCustomerName(id) {
@@ -57,6 +66,28 @@ export class ReservationComponent implements OnInit {
       }
     }
     return "";
+  }
+
+  filterAction() {
+    var reservationServiceList = this.reservationService.reservationList.slice(0);
+    var start_date = new  Date(this.data_start+"");
+    var end_date = new Date(this.data_end+"");
+    if(this.data_start == null || this.data_start == "") {
+      start_date = new Date();
+    }
+    if(this.data_end == null || this.data_end == "") {
+      end_date = new Date(9999, 12, 1);
+    }
+    var newList = [];
+    for(var i=0;i<reservationServiceList.length;i++) {
+      if(reservationServiceList[i].start_data >= start_date && reservationServiceList[i].end_data <= end_date) {
+        newList.push(reservationServiceList[i]);
+      }
+    }
+    this.reservationList = null; 
+    this.reservationList = newList; 
+    
+   
   }
 
 }
